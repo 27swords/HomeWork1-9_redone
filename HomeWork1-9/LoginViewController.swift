10ba40da92ccf2c99d24e6d9c84957de773ca694
Alexander Chervoncev <chervoncev2015@gmail.com>
2022-04-06 21:39:20 +0300

    Homework interface Part 2

--- HomeWork1-9/LoginViewController.swift
+++ HomeWork1-9/LoginViewController.swift
@@ -11,21 +11,63 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    @IBAction func loginButton(_ sender: Any) {
-
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+        // Login details
         let username = usernameTextField.text
         let password = passwordTextField.text
 
-        if username == "111" && password == "222" {
-            print("Welcome!")
+        if username == "111" && password == "111" {
+            print("Welcome")
         } else {
             print("Invalid username or password")
         }
     }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        let checkResult = checkUserData()
+        
+        if !checkResult {
+            showLoginError()
+        }
+        
+        return checkResult
+    }
+    // Password and login to log in to the system
+    func checkUserData() -> Bool {
+        
+        guard let userName = usernameTextField.text,
+              let password = passwordTextField.text else { return false }
+        
+        if userName == "111" && password == "111" {
+            return true
+        } else {
+            return false
+        }
+    }
+    // Alert with an error
+    func showLoginError() {
+        
+        let alert = UIAlertController(
+            title: "Error",
+            message: "Invalid username or password",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "Ok",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
---
+++ HomeWork1-9/MenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuViewController.swift
+//  HomeWork1-9
+//
+//  Created by Alexander Chervoncev on 6.04.22.
+//
+
+import UIKit
+
+class MenuViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
